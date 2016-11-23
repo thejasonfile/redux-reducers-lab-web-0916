8fc251d49db0be068c9f8f1341b6ca23627602c3
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-23 16:33:28 -0500

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,11 @@
-export default function ingredientReducer() {
+export default function ingredientReducer(state=[], action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return [...state, action.payload]
+    case 'REMOVE_INGREDIENT':
+      return [...state.slice(0, action.payload), ...state.slice(state.length-1)]
+    default:
+      return state;
+  }
 
 }
